2cc7b72fdb1476803706b424f6c9b53b0f6e724f
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-10-05 16:14:34 +1000

    Add files via upload

---
+++ shared/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- Job thats allowed to use taximeter
+Config.AllowedJobs = {'taxi', 'mechanic', 'police'} -- Add your allowed jobs here
+
+-- List of vehicles allowed to use taximeter
+Config.AllowedVehicles = {
+    "taxi",
+    "tourbus",
+    "patriot2",
+    "stretch"
+}
